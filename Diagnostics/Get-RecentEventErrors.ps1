69bfa0c5779430998df8bbe55be5eb3e4169ea87
Mohammed Asif <asasifshaikh668@gmail.com>
2026-01-02 20:35:41 +0530

    Add files via upload

---
+++ Diagnostics/Get-RecentEventErrors.ps1
@@ -0,0 +1,16 @@
+﻿function Get-EventLogErrors {
+    $LogNames = @("System", "Application")
+    
+    foreach ($Log in $LogNames) {
+        Write-Host "--- Last 50 Errors/Critical in $Log ---" -ForegroundColor Cyan
+        # Using Get-WinEvent to capture Critical (1) and Error (2) levels
+        Get-WinEvent -FilterHashtable @{
+            LogName = $Log
+            Level = 1, 2
+        } -MaxEvents 50 -ErrorAction SilentlyContinue | 
+        Select-Object TimeCreated, Id, LevelDisplayName, Message | 
+        Format-Table -AutoSize
+    }
+}
+
+Get-EventLogErrors
