2e61646219548cf84c727fb510e467eb09d5f8c3
Mohammed Asif <asasifshaikh668@gmail.com>
2026-01-02 21:05:12 +0530

    Add files via upload

---
+++ Diagnostics/Reset-PrintSpooler.bat
@@ -0,0 +1,5 @@
+@echo off
+net stop spooler
+del /Q /F /S "%systemroot%\System32\Spool\Printers\*.*"
+net start spooler
+pause
---
+++ Diagnostics/Reset-PrintSpooler.ps1
@@ -0,0 +1,5 @@
+﻿Stop-Service -Name "Spooler" -Force
+# Clear the spooler cache files
+Remove-Item -Path "$env:SystemRoot\System32\spool\PRINTERS\*" -Force
+Start-Service -Name "Spooler"
+Write-Host "Print Spooler has been reset successfully." -ForegroundColor Green
